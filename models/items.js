114584b5709ee0ad6068123cdf83441cb951777b
jonathan weinstein <jonathanweinstein@jonathans-MBP.home>
2019-06-07 20:42:04 -0400

    model change

--- models/items.js
+++ models/items.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose')
 
 const itemSchema = mongoose.Schema({
   name: {type: String, required: true},
-  artist: {type: String},
-  description: {type: String},
-  itemType: {type: String},
-  image: {type: String, default: "https://i.imgur.com/hz09qTy.png?1", required:true},
-  price: {type: Number}
+  artist: {type: String, required: true},
+  description: {type: String, required: true},
+  itemType: {type: String, required: true},
+  image: {type: String, required:true},
+  price: {type: Number, required: true}
 })
 
 module.exports = mongoose.model('Item', itemSchema)
